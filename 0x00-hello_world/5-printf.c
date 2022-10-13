8f9807fca4df372ec78177ad2a7462ee14e55dfc
JudyR93 <judy.raphesu@gmail.com>
2022-10-13 16:14:27 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,11 @@
  *
  * Description: Outputs text
  *
- * return: Always 0 (success)
+ * return: always 0 (success)
+ *
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
