fb1ab6fc88782ce980eef3642da153ca868f4e87
JudyR93 <judy.raphesu@gmail.com>
2022-10-13 14:51:14 -0700

    Task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Description : outputs text
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
