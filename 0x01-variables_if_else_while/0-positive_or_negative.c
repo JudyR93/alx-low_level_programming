d34b74271cba7f7c810d08bc510b7a22f4fd7731
JudyR93 <judy.raphesu@gmail.com>
2022-10-17 06:12:12 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,41 +1,29 @@
 #include <stdlib.h>
-
 #include <time.h>
-
+#include <stdio.h>
 /**
- * main -Entry
+ * main - Deterrmine if randon is int
  *
- * Description: checks positive,negative or zero
+ * Return: 0
  *
- * Return: always 0 (success)
 */
-/* betty style doc for function main goes there */
-
 int main(void)
+
 {
 
-
-	int n;
+		int n;
 
 
 
 			srand(time(0));
 
 				n = rand() - RAND_MAX / 2;
-
 				if (n > 0)
-				{
-				printf("%d is positive", n);
-				}
-				elseif(n < 0)
-				{
-				printf("%d is negative", n);
-				}
+					printf("%d is positive\n", n);
+				else if (n < 0)
+					printf("%d is negative\n", n);
 				else
-				{
-				printf("%d is zero", n);
-				}
+				  printf("%d is zero\n", n);
 				return (0);
 
 }
-
