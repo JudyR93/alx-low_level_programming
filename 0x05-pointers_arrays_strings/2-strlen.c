25e3b841dd5c0a5a7f40cd90b7e5b442c6b7fdb8
JudyR93 <judy.raphesu@gmail.com>
2022-10-26 02:52:15 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - outputs string length
+ * @s : a pointer starting 
+ *
+ * return :0
+ */
+int _strlen(char *s)
+{
+int length;
+while( *s =! '\0')
+     { 
+	length += 1;
+	*s = *s + 1 ;
+
+     }
+}
