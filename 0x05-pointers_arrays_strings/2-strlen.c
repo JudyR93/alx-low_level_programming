aefbd5ec872bbbd3026276396bf02a71adef0630
JudyR93 <judy.raphesu@gmail.com>
2022-10-26 04:05:12 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
- * _strlen - outputs string length
- * @s : a pointer starting 
+ * _strlen -outputs string length
+ * @s : a pointer starting
  *
- * return : length of string
+ * Return: the length
  */
 int _strlen(char *s)
 {
-	int len = 0;
-   while(*s != 0)
-     { 
-	len++;
-	s++;
-     }
-   return(len);
+int len = 0;
+while (*s != 0)
+{
+len++;
+s++;
+}
+return (len);
 }
