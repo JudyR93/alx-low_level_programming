248b67ce9e48ae405f063c5b52ec1a1f06cdcd1f
JudyR93 <judy.raphesu@gmail.com>
2022-10-26 04:35:57 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,12 +11,9 @@
 
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (*(str + 1) != '\0')
-	{
-		putchar(*(str + 1));
-	i++;
-	}
-	putchar(10);
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar("\n");
 }
