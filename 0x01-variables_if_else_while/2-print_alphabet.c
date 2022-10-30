03b138fbba13ebd8b553d59ddab0bcca2a341e1a
JudyR93 <judy.raphesu@gmail.com>
2022-10-30 06:12:26 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+ch = "a";
+while (ch <= "z")
+{
+putchar(ch);
+ch++;
+}
+putchar("\n");
+return (0);
+}
+
+
