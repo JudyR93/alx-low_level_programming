83456f20250c63430025d6a4b8029899600ebedb
JudyR93 <judy.raphesu@gmail.com>
2023-03-10 11:38:00 -0800

    Task 0x09

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - function prints string
+* @str: the text to be printed
+* Return: Always 0 (Success)
+*/
+
+void _puts(char *str)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
